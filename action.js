31228d9dc11ea72f236af8513358f6eef81f1ef3
Kevin Alwell <alwellkevin44@gmail.com>
2018-09-17 17:52:49 -0400

    transfer and update calls

--- action.js
+++ action.js
@@ -1,7 +1,32 @@
-var escalate = (callPurpose) => {
-    console.log("ESCALATING CALL: ", callPurpose);
+var Nexmo = require('nexmo');
+var nexmo = new Nexmo({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+    applicationId: process.env.APPLICATION_ID,
+    privateKey: process.env.PRIVATE_KEY_PATH,
+}, {
+    debug: true
+});
+
+var escalate = (callPurpose, conversation_uuid) => {
+    console.log("ESCALATING CALL: ", callPurpose, conversation_uuid);
+
+    var conversationId = process.env.CONFERENCE_NAME + conversation_uuid;
     //TODO: Connect SIP Leg.
 
+    nexmo.calls.update(conversationId, {
+        action: 'transfer',
+        destination: {
+            "type": "ncco",
+            "url": [process.env.BASE_URL + "/" + callPurpose]
+        }
+    }, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+    });
     //TODO: Disconnect IVR Leg.
 }
 
--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var nexmo = new Nexmo({
 }, {
     debug: true
 });
-
+var conversation_uuid;
 
 //*******NOTICE:********
 //*******HANDLES SINGLE SESSION ONLY. */
@@ -27,18 +27,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get("/answer", (req, res) => {
-    //dial out to dialogflow
-    dialogflowEnabled = true
-    fails = []
-    
     //TODO: Dynamic Lookup of customer record req.query.from in SF.
+    //req.query.from;
+    conversation_uuid = req.query.uuid;
 
+    //Dial out to Dialogflow
     res.send([{
-            "action": "talk",
-            "text": "Hello, thank you for contacting en vision. The worlds leading high end home theater retailer."
-        },
-        {
             "action": "connect",
+            "timeout": "1",
             "from": req.query.from,
             "endpoint": [{
                 "type": "phone",
@@ -47,7 +43,7 @@ app.get("/answer", (req, res) => {
         },
         {
             "action": "conversation",
-            "name": process.env.CONFERENCE_NAME,
+            "name": process.env.CONFERENCE_NAME + conversation_uuid,
             "endOnExit": "true"
         }
     ])
@@ -55,34 +51,71 @@ app.get("/answer", (req, res) => {
 
 app.post("/event", (req, res) => {
     console.log("In event endpoint: ", req.body);
+
+    res.send([{
+        "action": "conversation",
+        "name": process.env.CONFERENCE_NAME + conversation_uuid,
+        "startOnEnter": "false",
+    }])
+})
+
+
+//EXAMPLE ACTION:
+
+//     In Google endpoint:  { responseId: 'f10bb322-d48e-418c-ad20-c41e2cc3cd4c',
+//   queryResult: 
+//    { queryText: 'I\'m not a customer yet',
+//      action: 'contact-sales-agent',
+//      parameters: {phone:"17326157295"},
+//      allRequiredParamsPresent: true,
+//      fulfillmentText: 'Okay, I am going to connect you with a Sales agent. Please wait for a moment while I find someone who is available.',
+//      fulfillmentMessages: [ [Object] ],
+//      intent: 
+//       { name: 'projects/dreamforce-voice-bot-2018/agent/intents/7c62dc72-24f8-41b9-b772-f4c1ef750d37',
+//         displayName: 'contact-sales-agent' },
+//      intentDetectionConfidence: 0.8,
+//      languageCode: 'en-us' },
+//   originalDetectIntentRequest: { source: 'GOOGLE_TELEPHONY', payload: { telephony: [Object] } },
+//   session: 'projects/dreamforce-voice-bot-2018/agent/sessions/756t5aSJQzae-pGJowa2lg' }
+
+// CALLED FROM DIALOG FLOW ACTION
+app.all("/google", (req, res) => {
+    console.log("************************************")
+    console.log("GOOGLE REQ: ", req.body);
+    console.log("************************************")
+    
+    //TODO:IDENTIFY DYNAMIC CONVERSATION
+    //originalDetectIntentRequest: { source: 'GOOGLE_TELEPHONY', payload: { telephony: [Object] } }, <-- Need enterprise account to get this object. Otherwise, redacted.
+
+    actions.escalate(req.body.queryResult.action, conversation_uuid);
+
     res.sendStatus(200)
 })
 
-app.all("/google", (req, res) => {
-    console.log("In Google endpoint: ", req.body, req.body.originalDetectIntentRequest.payload.telephony);
-    console.log("************************************")
-    console.log("GOOGLE REQ: ", req);
-    console.log("************************************")
-    //TODO:IDENTIFY CONVERSATION
 
-    //     In Google endpoint:  { responseId: 'f10bb322-d48e-418c-ad20-c41e2cc3cd4c',
-    //   queryResult: 
-    //    { queryText: 'I\'m not a customer yet',
-    //      action: 'contact-sales-agent',
-    //      parameters: {},
-    //      allRequiredParamsPresent: true,
-    //      fulfillmentText: 'Okay, I am going to connect you with a Sales agent. Please wait for a moment while I find someone who is available.',
-    //      fulfillmentMessages: [ [Object] ],
-    //      intent: 
-    //       { name: 'projects/dreamforce-voice-bot-2018/agent/intents/7c62dc72-24f8-41b9-b772-f4c1ef750d37',
-    //         displayName: 'contact-sales-agent' },
-    //      intentDetectionConfidence: 0.8,
-    //      languageCode: 'en-us' },
-    //   originalDetectIntentRequest: { source: 'GOOGLE_TELEPHONY', payload: { telephony: [Object] } },
-    //   session: 'projects/dreamforce-voice-bot-2018/agent/sessions/756t5aSJQzae-pGJowa2lg' }
-    actions.escalate(req.body.queryResult.action);
+// ACTIONS BELOW
+app.all("/contact-sales-agent", (req, res) => {
+    res.json({
+        "action": "connect",
+        "timeout": "1",
+        "from": req.query.from,
+        "endpoint": [{
+            "type": "phone",
+            "number": process.env.SALES_NUMBER
+        }]
+    })
+})
 
-    res.sendStatus(200)
+app.all("/contact-customer-service", (req, res) => {
+    res.json({
+        "action": "connect",
+        "timeout": "1",
+        "from": req.query.from,
+        "endpoint": [{
+            "type": "sip",
+            "number": process.env.SUPPORT_NUMBER
+        }]
+    })
 })
 
 // Start server
