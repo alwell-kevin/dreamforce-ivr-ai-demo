c9644de7b540a3b833e5839e5c4e7b18dcf96614
Kevin Alwell <alwellkevin44@gmail.com>
2018-09-17 15:40:05 -0400

    init

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var Nexmo = require('nexmo');
+var nexmo = new Nexmo({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+    applicationId: process.env.APPLICATION_ID,
+    privateKey: process.env.PRIVATE_KEY_PATH,
+}, {
+    debug: true
+});
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/answer", (req, res) => {
+    //dial out to dialogflow
+    dialogflowEnabled = true
+    fails = []
+    res.send([
+      {
+        "action": "talk",
+        "text": "Hello, please wait while we connect you"
+      },
+      {
+        "action": "connect",
+        "timeout": "1",
+        "from": req.query.from,
+        "endpoint": [
+          {
+            "type": "phone",
+            "number": process.env.DIALOGFLOW_NUMBER
+          }
+        ]
+      },
+      {
+        "action": "conversation",
+        "name": "test-conference",
+        "endOnExit": "true"
+      }
+    ])
+  })
+
+  app.post("/events", (req, res) => {
+    console.log("events", req.body);
+    res.sendStatus(200)
+  })
+
+  // Start server
+app.listen(port, () => {
+    console.log('Express server started on port ' + port);
+})
